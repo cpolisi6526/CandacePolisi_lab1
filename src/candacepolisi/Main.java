da4110388860abc71ca8c7009ba2c21132710888
BT_1N3_18 <cpolisi6526@bths.edu>
2018-10-11 08:37:30 -0400

    3

--- src/candacepolisi/Main.java
+++ src/candacepolisi/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hey whats your name?");
+        String name= input.nextLine();
+        System.out.println("Nice to meet you  "+ name+ ".  Pick a game: type a for me to guess your number or type b for you to guess my number ");
         // write your code here
+        String game= input.nextLine();
+        if (game.equals("a")){
         String response = "";
         int min = 1;
         int max = 100;
         int guess;
-        Scanner input = new Scanner(System.in);
+
         System.out.println("think of a number 1-100. Okay? Now let me try to guess it.");
         while (!(response.equals("correct"))) {
             guess = (min + max) / 2;
@@ -33,19 +39,24 @@ public class Main {
                 System.out.println("invalid input type higher, lower, or correct to continue");
             }
         }
-    }
-
-
-    public static void guesser1(int num1) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("guess the secret number");
-        int guess = input.nextInt();
-        while ((guess > num1) || (guess < num1)) {
-            System.out.println("guess again noob");
-            guess = input.nextInt();
+     }
+     if (game.equals("b")){
+         double num= Math.random();
+         int num1= 100*num;
+         System.out.println("guess the secret number 1-100");
+         int guess = input.nextInt();
+         while ((guess > num1) || (guess < num1)) {
+             System.out.println("guess again noob");
+             guess = input.nextInt();
+         }
+         System.out.println("You got it dude! The number was indeed  " + num1);
+     }
+     else{
+         System.out.println("not a valid input type either a or b ");
+     }
         }
-        System.out.println("You got it dude! The number was indeed  " + num1);
-    }
+
+
 }
 
 
