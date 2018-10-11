34246f734639edf88244981c456f8a703598b113
BT_1N3_18 <cpolisi6526@bths.edu>
2018-10-11 08:25:28 -0400

    2

--- src/candacepolisi/Main.java
+++ src/candacepolisi/Main.java
@@ -5,46 +5,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        // (guesser1(5));
+        // write your code here
+        String response = "";
+        int min = 1;
+        int max = 100;
+        int guess;
+        Scanner input = new Scanner(System.in);
+        System.out.println("think of a number 1-100. Okay? Now let me try to guess it.");
+        while (!(response.equals("correct"))) {
+            guess = (min + max) / 2;
+            System.out.println("hey is ur number  " + guess + "  respond w correct, higher, or lower ");
+            response = input.nextLine();
+            if ((response.equals("correct")) || (response.equals("lower")) || (response.equals("higher"))) {
+                if (response.equals("correct")) {
+                    System.out.println("yay i knew your number was  " + guess);
+                } else {
+                    if (response.equals("higher")) {
+                        System.out.println("darn let me guess again");
+                        min = guess;
+                    }
+                    if (response.equals("lower")) {
+                        System.out.println("darn let me guess again");
+                        max = guess;
+                    }
+                }
+            } else {
+                System.out.println("invalid input type higher, lower, or correct to continue");
+            }
+        }
     }
 
-    public static void guesser1(int num1){
-        Scanner input= new Scanner(System.in);
+
+    public static void guesser1(int num1) {
+        Scanner input = new Scanner(System.in);
         System.out.println("guess the secret number");
-        int guess= input.nextInt();
-        while ((guess> num1) || (guess<num1)){
+        int guess = input.nextInt();
+        while ((guess > num1) || (guess < num1)) {
             System.out.println("guess again noob");
-            guess= input.nextInt();
+            guess = input.nextInt();
         }
         System.out.println("You got it dude! The number was indeed  " + num1);
     }
-
-    public static void guesser2(String [] args){
-        String response= "";
-       Scanner input= new Scanner(System.in);
-        System.out.println("think of a number 1-100. Okay? Now let me try to guess it.");
-        double g= Math.random();
-        int g2= 100g;
-        System.out.println("is it  " + g2 + "respond w higher, lower, or correct");
-        while (!(response.equals("correct")))
-        response= input.nextLine();
-        if (response.equals("correct") || response.equals("lower") || response.equals("higher")){
-            if (response.equals("correct")) {
-                System.out.println("knew i'd get it. ofc the number was  " + g2);
-            }
-            else{
-                if (response.equals("higher")){
-                System.out.println("darn, lemme guess again");
-                g= Math.random();
-                g2= g2 + 100*g;
-                }
-                if (response.equals("lower")){
-                    System.out.println("darn, lemme guess again");
-                    g= Math.random();
-                    g2= g2- 100g;
-                }
-            }
-        }
-    }
 }
+
